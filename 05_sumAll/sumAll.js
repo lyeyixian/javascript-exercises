338117c04f8a1d22ccf00f04270528b63657dd5c
Lye Yi Xian <lyeyx999@hotmail.com>
2021-12-31 13:55:49 +0800

    Complete 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    let sum = 0;
 
+    if (from < 0 || to < 0 || typeof from !== 'number' || typeof to !== 'number') {
+        return 'ERROR'
+    }
+
+    if (from < to) {
+        for (let i = from; i <= to; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = from; i >= to; i--) {
+            sum += i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
