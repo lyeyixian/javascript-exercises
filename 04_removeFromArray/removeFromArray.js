a45c7576730268a4690104886fd63a89658b26e3
Lye Yi Xian <lyeyx999@hotmail.com>
2021-12-31 13:43:39 +0800

    Complete 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    let res = arr;
 
+    toRemove.forEach(element => res = res.filter(arrElement => arrElement !== element));
+
+    return res;
 };
 
 // Do not edit below this line
