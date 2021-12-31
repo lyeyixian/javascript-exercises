138e7ab990c7b767370fd7098cfe3f8b1cca582c
Lye Yi Xian <lyeyx999@hotmail.com>
2021-12-31 17:20:07 +0800

    Complete 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+    return parseFloat(((fahrenheit - 32) * 5 / 9).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+    return parseFloat((celcius * 9 / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
