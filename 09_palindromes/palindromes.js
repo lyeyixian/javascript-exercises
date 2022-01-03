ec5076b65b66e13fb5a334c08fbb2fd96eb59b61
Lye Yi Xian <lyeyx999@hotmail.com>
2022-01-03 23:41:44 +0800

    Complete 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    processed = str.toLowerCase().replace(/[^A-Za-z]/g, '');
 
+    return processed.split("").reverse().join("") === processed;
 };
 
 // Do not edit below this line
