2bbedec5cfdd845bb07c7413ea30e48d9e8dbd04
Lye Yi Xian <lyeyx999@hotmail.com>
2022-01-03 23:35:34 +0800

    Complete 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.length ? arr.reduce((a, b) => a * b) : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	total = 1;
+
+  for (let i = 1; i <= n; i++) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
