7896049206e9d7f2fb7f9890a0f1ba96a3f15863
Lye Yi Xian <lyeyx999@hotmail.com>
2022-01-03 23:46:51 +0800

    Complete 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let parse = parseInt(n);
 
+    if (parse < 0) {
+        return 'OOPS';
+    }
+
+    let a = 1;
+    let b = 1;
+
+    for (let i = 2; i < parse; i++) {
+        let temp = a;
+        a = b;
+        b = temp + b;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
