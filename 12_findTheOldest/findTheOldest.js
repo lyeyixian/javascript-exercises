57933c5b2e91606a25720fd18d64abfe4ebb38ff
Lye Yi Xian <lyeyx999@hotmail.com>
2022-01-03 23:58:06 +0800

    Complete 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(birth, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
 
+        return death - birth;
+    }
+
+    return people.reduce((prev, current) => {
+        const prevAge = getAge(prev.yearOfBirth, prev.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        return prevAge < currentAge ? current : prev;
+    })
 };
 
 // Do not edit below this line
